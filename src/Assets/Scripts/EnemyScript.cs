950e5cc6d822c0d5c10725b946ade8dda6d2afc6
Paul Siicu <psiicu@hotmail.com>
2014-05-09 13:47:02 -0700

    enemies deactivate outside camera, improved scrolling, player contained within camera bounds
    tybg

--- src/Assets/Scripts/EnemyScript.cs
+++ src/Assets/Scripts/EnemyScript.cs
@@ -3,23 +3,70 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+	private bool hasSpawn;
+	private MoveScript moveScript;
 	private WeaponScript[] weapons;
 
 	void Awake()
 	{
 		// retrieve all the weapons in child objects once
 		weapons = GetComponentsInChildren<WeaponScript>();
+
+		// retrieve scripts to disable when not spawned
+		moveScript = GetComponent<MoveScript>();
+	}
+
+	void Start()
+	{
+		hasSpawn = false;
+
+		collider2D.enabled = false;
+		moveScript.enabled = false;
+
+		foreach (WeaponScript weapon in weapons)
+			weapon.enabled = false;
 	}
 
 	void Update()
 	{
-		foreach (WeaponScript weapon in weapons)
+		// check if spawned
+		if (hasSpawn == false)
+		{
+			if (renderer.IsVisibleFrom(Camera.main))
+			{
+				Spawn();
+			}
+		}
+		else
 		{
 			// auto-fire
-			if (weapon != null && weapon.CanAttack)
+			foreach (WeaponScript weapon in weapons)
 			{
-				weapon.Attack (true);
+				if (weapon != null && weapon.enabled && weapon.CanAttack)
+				{
+					weapon.Attack(true);
+				}
 			}
+
+			// out of the camera? destroy the game object
+			if (renderer.IsVisibleFrom(Camera.main) == false)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	private void Spawn()
+	{
+		hasSpawn = true;
+
+		// enable everything
+		collider2D.enabled = true;
+		moveScript.enabled = true;
+
+		foreach (WeaponScript weapon in weapons)
+		{
+			weapon.enabled = true;
 		}
 	}
 }
